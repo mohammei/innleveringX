1355838d521ffa967fc45c70e10c018318a70189
mohammei <mohammedismal@hotmail.com>
2015-05-26 23:57:35 +0200

    added til account på urlsen

--- djangoprojects/mysite/useraccount/urls.py
+++ djangoprojects/mysite/useraccount/urls.py
@@ -3,5 +3,8 @@ from useraccount import views
 urlpatterns = patterns('',
 url(r'^register$', views.user_register, name='user_register'),
 url(r'^login$', views.user_login, name='user_login'),
+url(r'^account$', views.user_account, name='user_account'),
+
+
 
 )
