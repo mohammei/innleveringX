fd5b03ee29399bdf73e77af2988b98fa0f6f4403
mohammei <mohammedismal@hotmail.com>
2015-05-26 18:46:27 +0200

    fredig laget register side med html og views som funker

--- djangoprojects/mysite/useraccount/urls.py
+++ djangoprojects/mysite/useraccount/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
 from useraccount import views
 urlpatterns = patterns('',
-
+url(r'^register$', views.user_register, name='user_register'),
 )
--- djangoprojects/mysite/useraccount/views.py
+++ djangoprojects/mysite/useraccount/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.shortcuts import redirect
 
+def user_register(request):
+    context = {}
+    if request.method == "POST":
+        user = User()
+        user.first_name = request.POST.get('firstname')
+        user.last_name = request.POST.get('lastname')
+        user.username = request.POST.get('username')
+        user.email = request.POST.get('email')
+        user.set_password(request.POST.get('password'))
+        user.save()
+    return render(request, 'useraccount/register.html', context)
+
 # Create your views here.
